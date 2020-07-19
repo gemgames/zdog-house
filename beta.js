1920d6111d7ce4e9d9078bcec2122a34e9246bd0
gemgames <61914748+gemgames@users.noreply.github.com>
2020-07-19 03:36:46 -0400

    Update beta.js

--- beta.js
+++ beta.js
@@ -343,50 +343,50 @@ var debug = function(p) {
 var subModes = function(p) {
   if (mode.move) {
     doc.redIcon.src = dimension.red
-      ? "/icons/" + style + "/redH.svg"
-      : "/icons/" + style + "/red.svg";
+      ? "icons/" + style + "/redH.svg"
+      : "icons/" + style + "/red.svg";
     doc.greenIcon.src = dimension.green
-      ? "/icons/" + style + "/greenH.svg"
-      : "/icons/" + style + "/green.svg";
+      ? "icons/" + style + "/greenH.svg"
+      : "icons/" + style + "/green.svg";
     doc.blueIcon.src = dimension.blue
-      ? "/icons/" + style + "/blueH.svg"
-      : "/icons/" + style + "/blue.svg";
+      ? "icons/" + style + "/blueH.svg"
+      : "icons/" + style + "/blue.svg";
   }
   if (mode.rotate) {
     doc.redIcon.src = dimension.red
-      ? "/icons/" + style + "/redRH.svg"
-      : "/icons/" + style + "/redR.svg";
+      ? "icons/" + style + "/redRH.svg"
+      : "icons/" + style + "/redR.svg";
     doc.greenIcon.src = dimension.green
-      ? "/icons/" + style + "/greenRH.svg"
-      : "/icons/" + style + "/greenR.svg";
+      ? "icons/" + style + "/greenRH.svg"
+      : "icons/" + style + "/greenR.svg";
     doc.blueIcon.src = dimension.blue
-      ? "/icons/" + style + "/blueRH.svg"
-      : "/icons/" + style + "/blueR.svg";
+      ? "icons/" + style + "/blueRH.svg"
+      : "icons/" + style + "/blueR.svg";
   }
   if (mode.size) {
     doc.redIcon.src = dimension.red
-      ? "/icons/" + style + "/heightH.svg"
-      : "/icons/" + style + "/height.svg";
+      ? "icons/" + style + "/heightH.svg"
+      : "icons/" + style + "/height.svg";
     doc.greenIcon.src = dimension.green
-      ? "/icons/" + style + "/depthH.svg"
-      : "/icons/" + style + "/depth.svg";
+      ? "icons/" + style + "/depthH.svg"
+      : "icons/" + style + "/depth.svg";
     doc.blueIcon.src = dimension.blue
-      ? "/icons/" + style + "/widthH.svg"
-      : "/icons/" + style + "/width.svg";
+      ? "icons/" + style + "/widthH.svg"
+      : "icons/" + style + "/width.svg";
   }
 };
 var clickMode = function(p) {
   mode = { size: false, move: false, rotate: false };
   mode[p] = true;
   doc.moveIcon.src = mode.move
-    ? "/icons/" + style + "/moveH.svg"
-    : "/icons/" + style + "/move.svg";
+    ? "icons/" + style + "/moveH.svg"
+    : "icons/" + style + "/move.svg";
   doc.rotateIcon.src = mode.rotate
-    ? "/icons/" + style + "/rotateH.svg"
-    : "/icons/" + style + "/rotate.svg";
+    ? "icons/" + style + "/rotateH.svg"
+    : "icons/" + style + "/rotate.svg";
   doc.sizeIcon.src = mode.size
-    ? "/icons/" + style + "/sizeH.svg"
-    : "/icons/" + style + "/size.svg";
+    ? "icons/" + style + "/sizeH.svg"
+    : "icons/" + style + "/size.svg";
   subModes();
 };
 var click3D = function(p) {
@@ -733,23 +733,23 @@ var loadElements = function() {
     Bb +
     ' onclick="loadElementData(' +
     0 +
-    ')"><img src="/icons/elements/camera.svg"><span class="element-name">Camera</span></div>' +
+    ')"><img src="icons/elements/camera.svg"><span class="element-name">Camera</span></div>' +
     "</div>";
 
   var renderFolder = function(n) {
     Aa = Aa.concat(
-      '<div class="elementName"><img src="/icons/elements/' +
+      '<div class="elementName"><img src="icons/elements/' +
         fileData.groups[n].type +
         '.svg"><div class="moreDropdown"><span class="element-name">' +
         fileData.groups[n].name +
-        '</span><img class="moreIcon" src="/icons/elements/more.svg">' +
+        '</span><img class="moreIcon" src="icons/elements/more.svg">' +
         '<img class="more-content" onclick="nameGroup(' +
         n +
-        ')"src="/icons/file/name.svg"><img class="more-content" onclick="duplicateGroup(' +
+        ')"src="icons/file/name.svg"><img class="more-content" onclick="duplicateGroup(' +
         n +
-        ')"src="/icons/file/duplicate.svg"><img class="more-content" onclick="deleteGroup(' +
+        ')"src="icons/file/duplicate.svg"><img class="more-content" onclick="deleteGroup(' +
         n +
-        ')" src="/icons/file/delete.svg"></div></div>'
+        ')" src="icons/file/delete.svg"></div></div>'
     );
   };
   var renderElement = function(n) {
@@ -762,19 +762,19 @@ var loadElements = function() {
         n +
         ')">' +
         Dd +
-        '<img src="/icons/elements/' +
+        '<img src="icons/elements/' +
         elements[n].type +
         '.svg"><div class="moreDropdown"><span class="element-name">' +
         elements[n].name +
-        '</span><img class="moreIcon" src="/icons/elements/more.svg">' +
+        '</span><img class="moreIcon" src="icons/elements/more.svg">' +
         '<img class="more-content" onclick="duplicateElement(' +
         n +
-        ')"src="/icons/file/duplicate.svg"><img class="more-content" onclick="deleteElement(' +
+        ')"src="icons/file/duplicate.svg"><img class="more-content" onclick="deleteElement(' +
         n +
-        ')" src="/icons/file/delete.svg">' +
+        ')" src="icons/file/delete.svg">' +
         '<img class="more-content" onclick="moveElement(' +
         n +
-        ')"src="/icons/file/move.svg">' +
+        ')"src="icons/file/move.svg">' +
         "</div></div>"
     );
   };
@@ -1240,7 +1240,6 @@ var changeElementData = function() {
     }
   }
 };
-
 //sidebar data
 var changeSidebarData = function() {
   fileData.name = doc.gebi("sidebar-name").value;
@@ -1269,36 +1268,36 @@ var updateStyle = function() {
     directionalCubeElement.frontFace = "#E62";
     directionalCubeElement.rearFace = "#E62";
   }
-  doc.gebi("edIRed").src = "/icons/" + style + "/red.svg";
-  doc.gebi("edIGreen").src = "/icons/" + style + "/green.svg";
-  doc.gebi("edIBlue").src = "/icons/" + style + "/blue.svg";
-  doc.gebi("edIRedR").src = "/icons/" + style + "/redR.svg";
-  doc.gebi("edIGreenR").src = "/icons/" + style + "/greenR.svg";
-  doc.gebi("edIBlueR").src = "/icons/" + style + "/blueR.svg";
-  doc.gebi("edIHeight").src = "/icons/" + style + "/height.svg";
-  doc.gebi("edIDepth").src = "/icons/" + style + "/depth.svg";
-  doc.gebi("edIWidth").src = "/icons/" + style + "/width.svg";
-  doc.gebi("sidebar-RedR").src = "/icons/" + style + "/redR.svg";
-  doc.gebi("sidebar-GreenR").src = "/icons/" + style + "/greenR.svg";
-  doc.gebi("sidebar-BlueR").src = "/icons/" + style + "/blueR.svg";
+  doc.gebi("edIRed").src = "icons/" + style + "/red.svg";
+  doc.gebi("edIGreen").src = "icons/" + style + "/green.svg";
+  doc.gebi("edIBlue").src = "icons/" + style + "/blue.svg";
+  doc.gebi("edIRedR").src = "icons/" + style + "/redR.svg";
+  doc.gebi("edIGreenR").src = "icons/" + style + "/greenR.svg";
+  doc.gebi("edIBlueR").src = "icons/" + style + "/blueR.svg";
+  doc.gebi("edIHeight").src = "icons/" + style + "/height.svg";
+  doc.gebi("edIDepth").src = "icons/" + style + "/depth.svg";
+  doc.gebi("edIWidth").src = "icons/" + style + "/width.svg";
+  doc.gebi("sidebar-RedR").src = "icons/" + style + "/redR.svg";
+  doc.gebi("sidebar-GreenR").src = "icons/" + style + "/greenR.svg";
+  doc.gebi("sidebar-BlueR").src = "icons/" + style + "/blueR.svg";
   doc.moveIcon.src = mode.move
-    ? "/icons/" + style + "/moveH.svg"
-    : "/icons/" + style + "/move.svg";
+    ? "icons/" + style + "/moveH.svg"
+    : "icons/" + style + "/move.svg";
   doc.rotateIcon.src = mode.rotate
-    ? "/icons/" + style + "/rotate.svg"
-    : "/icons/" + style + "/rotate.svg";
+    ? "icons/" + style + "/rotate.svg"
+    : "icons/" + style + "/rotate.svg";
   doc.sizeIcon.src = mode.size
-    ? "/icons/" + style + "/sizeH.svg"
-    : "/icons/" + style + "/size.svg";
+    ? "icons/" + style + "/sizeH.svg"
+    : "icons/" + style + "/size.svg";
   doc.redIcon.src = dimension.red
-    ? "/icons/" + style + "/redH.svg"
-    : "/icons/" + style + "/red.svg";
+    ? "icons/" + style + "/redH.svg"
+    : "icons/" + style + "/red.svg";
   doc.greenIcon.src = dimension.green
-    ? "/icons/" + style + "/greenH.svg"
-    : "/icons/" + style + "/green.svg";
+    ? "icons/" + style + "/greenH.svg"
+    : "icons/" + style + "/green.svg";
   doc.blueIcon.src = dimension.blue
-    ? "/icons/" + style + "/blueH.svg"
-    : "/icons/" + style + "/blue.svg";
+    ? "icons/" + style + "/blueH.svg"
+    : "icons/" + style + "/blue.svg";
 };
 
 //camera mode and controls
